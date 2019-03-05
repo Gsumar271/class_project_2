1bdf13104896f97095ef98c65fe443c805fc4b3d
GeneSumar <statsrats@yahoo.com>
2019-03-05 15:43:29 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,34 +14,18 @@
 using classProgram::DBLList;
 using namespace std;
 
-// function prototype
-int BinarySearch(
-            const int a[ ],
-            int first,
-            int size,
-            int target,
-            int& found,
-            int& location
-            );
-void InsertList(string name, int weight);
-
 
 int main( )
 {
-    /*
-    const int size = 10;
-    int someArray[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int target;
-    int found;
-    int location;
-    */
     string name;
     int weight;
     DBLList nameList;
     
-
+    cout << "Please type a name and weight: " << endl;
+    
+    //get all the names from the console input
     for (int i=0; i<15; i++){
-        cout << "Please type a name and weight: " << endl;
+
         cin >> name;
         cin >> weight;
         nameList.insertNode(name, weight);
@@ -50,47 +34,9 @@ int main( )
     if(nameList.size()!= 0 )
         nameList.printList();
 
-    
+
 }
 
-void InsertList(string name, int weight){
-    
-}
-
-int BinarySearch(
-            const int a[ ],
-            int first,
-            int size,
-            int target,
-            int& found,
-            int& location
-            )
-{
-    int middle;
-    
-    //if array size = 0 target was not found
-    //otherwise divide array in 2 parts and search them
-    if (size == 0) {
-        found = -1;
-    }
-    else
-    {
-        middle = first + size/2;
-        if (target == a[middle])
-        {
-            location = middle;
-            found = location;
-        }
-        else if (target < a[middle])
-            // The target is less than a[middle], so search before the middle
-            BinarySearch(a, first, size/2, target, found, location);
-        else
-            // The target must be greater than a[middle], so search after the middle
-            BinarySearch(a, middle+1, (size-1)/2, target, found, location);
-    }
-    
-    return found;
-}
 
     
     
